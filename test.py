e5a91997c828b962518cdafc16c2063981b253ba
Kaboom154 <owen.riley08@gmail.com>
2025-10-21 09:50:03 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+def getIntegers(myList):
+    # myList : expected list or tuple
+    integers = []
+    # iterate through myList and add all the integers to the new list
+    if type() == int:
+        integers.append()
+    
+    return integers
+
