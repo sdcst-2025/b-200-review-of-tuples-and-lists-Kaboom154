3d6c71ef2e4aef5c5f702834de2deb943ea8ad93
Kaboom154 <owen.riley08@gmail.com>
2025-10-23 16:47:47 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,24 +1,11 @@
-'''
+
 def getIntersection(list1,list2):
-    # list 1: expected list or tuple
-    # list 2: expected list or tuple
-    # return a sorted list of numbers that is in both lists
-    # the intersection of the 2 number sets
-    
     common = [i for i in list1 if i in list2]
     common.sort()
     print(common)
     return common
-'''
-'''
-def getMerge(list1,list2):
-    # list 1: expected list or tuple
-    # list 2: expected list or tuple
-    # add the elements of list2 into list1
-    # if the list2 element is in list1, add it at the position where it occurs in list1
-    # if the list2 element is not in list1, add it to the end
 
-    #iterate through list2, index the spot of each same one in list1 and insert it
+def getMerge(list1,list2):
     for i in list2:
         if i in list1:
             pos1 = list1.index(i)
@@ -28,17 +15,10 @@ def getMerge(list1,list2):
         else:
             list1.append(i)
 
-    print(list2)
-    print(list1)
     return list1
-'''
+
 
 def getUnion(list1,list2):
-    # list 1: expected list or tuple
-    # list 2: expected list or tuple
-    # return a sorted list of numbers that is in either of the lists
-    # duplicate values will be ignored
-    
     union = list1 + list2
     union.sort()
     for i in union:
@@ -46,8 +26,6 @@ def getUnion(list1,list2):
             pos1 = union.index(i)
             union.pop(pos1)
 
-
-    print(union)
     return union 
 
 
